2feaf5274b845adba1f6c8501dac3f741c97c911
Vitaliy Fundovoy <nickvi@bk.ru>
2022-01-18 21:16:51 +0300

    BunTest have been written

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,16 +1,32 @@
 package praktikum;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class BunTest {
 
-    @Test
-    public void getName() {
+    private Bun testedBun;
+    private String expectedNameBun;
+    private float expectedPriceBun;
+
+    @Before
+    public void BunInit() {
+        expectedNameBun = "testBun";
+        expectedPriceBun = 150.0F;
+        testedBun = new Bun(expectedNameBun, expectedPriceBun);
     }
 
     @Test
-    public void getPrice() {
+    public void getNameReturnsName() {
+        String actualNameBun = testedBun.getName();
+        assertEquals(expectedNameBun, actualNameBun);
+    }
+
+    @Test
+    public void getPriceReturnsPrice() {
+        float actualPriceBun = testedBun.getPrice();
+        assertEquals(expectedPriceBun, actualPriceBun, 0.001);
     }
 }
