32488a5c27b5a7eddf51919c95f10930d844325e
Vitaliy Fundovoy <nickvi@bk.ru>
2022-01-26 00:17:45 +0300

    BunData added

---
+++ src/test/java/praktikum/dataObjects/BunData.java
@@ -0,0 +1,14 @@
+package praktikum.dataObjects;
+
+public class BunData {
+    private static final float BUN_PRICE = 1F;
+    private static final String BUN_NAME = "bun";
+
+    public static float getBunPrice() {
+        return BUN_PRICE;
+    }
+    
+    public static String getBunName() {
+        return BUN_NAME;
+    }
+}
